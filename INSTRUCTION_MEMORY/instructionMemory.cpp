1ec08724f5822e91d224280f6ecb83a7e6d90b15
Nelson Vivas <nelsonandresvivaslobo@gmail.comAchievements>
2022-10-25 21:24:51 -0400

    Se agrego funcion para transformar de string a int y map para opcode

--- INSTRUCTION_MEMORY/instructionMemory.cpp
+++ INSTRUCTION_MEMORY/instructionMemory.cpp
@@ -1,4 +1,34 @@
 #include "instructionMemory.h"
+#define charToInt(x) x - '0'
+using namespace std;
+set<string> rType = {"add", "sub", "sll", "or", "and", "slt" };
+set<string> lType = {"addi", "slli", "ori", "slti", "lw", "jalr"};
+set<string> sType = {"sw"};
+set<string> sbType = {"beq", "bne", "blt"};
+set<string> ubType = {"jal"};
+
+
+map<string, string> opCodeMap = {
+    {"add" , "0110011"},{"sub"  , "0110011"}, {"sll", "0110011"}, {"or"  , "0110011"}, {"and", "0110011"}, {"slt" , "0110011"},
+    {"addi", "0010011"}, {"slli", "0010011"}, {"ori", "0010011"}, {"slti", "0010011"}, {"lw" , "0000011"}, {"jalr", "1100111"},
+    {"sw"  , "0100011"},
+    {"beq" , "1100111"}, {"bne" , "1100111"}, {"blt", "1100111"},
+    {"jal" , "1101111"} 
+};
+
+
+int stringNumberToInt(const string& str) {
+    int decimals = str.length();
+    int res = 0;
+    int k = 1;
+    for(int i = decimals-1; i>=0; i--) {
+        res += k*charToInt(str[i]);
+        k *=10;
+    }
+
+
+    return res;
+}
 
 InstructionMemory::InstructionMemory(sc_module_name modName):
     sc_module(modName),
@@ -18,6 +48,8 @@ void InstructionMemory::process() {
     this->parseInstruction(instruction, wordList);
 
     string functType = this->getFunctionType(wordList[0]);
+
+
 }
 
 string InstructionMemory::getInstruction() {
@@ -59,7 +91,12 @@ string InstructionMemory::getFunctionType(const string& funct) {
         return "sb";
     else if(ubType.find(funct) != ubType.end())
         return "ub";
-    else
-        return "null";
     
+    return "null";
+    
+}
+void InstructionMemory::encodeInstruction(const string& instType, const vector<string>& wordList) {
+    if(instType == "r"){
+
+    }
 }
--- INSTRUCTION_MEMORY/instructionMemory.h
+++ INSTRUCTION_MEMORY/instructionMemory.h
@@ -6,48 +6,11 @@
 #include <string>
 //TODO: Leer atchivo .asc
 #include <map>
-#include <set>
 
-set<string> rType = {"add", "sub", "sll", "or", "and", "slt" };
-set<string> lType = {"addi", "slli", "ori", "slti", "lw", "jalr"};
-set<string> sType = {"sw"};
-set<string> sbType = {"beq", "bne", "blt"};
-set<string> ubType = {"jal"};
+#include <bits/stdc++.h>
 
-map<string, int> registerMap {
-    {"x0",0},
-    {"x1",1},
-    {"x2",2},
-    {"x3",3},
-    {"x4",4},
-    {"x5",5},
-    {"x6",6},
-    {"x7",7},
-    {"x8",8},
-    {"x9",9},
-    {"x10",10},
-    {"x11",11},
-    {"x12",12},
-    {"x13",13},
-    {"x14",14},
-    {"x15",15},
-    {"x16",16},
-    {"x17",17},
-    {"x18",18},
-    {"x19",19},
-    {"x20",20},
-    {"x21",21},
-    {"x22",22},
-    {"x23",23},
-    {"x24",24},
-    {"x25",25},
-    {"x26",26},
-    {"x27",27},
-    {"x28",28},
-    {"x29",29},
-    {"x30",30},
-    {"x31",31},
-}
+
+
 /*
     FUNCIONES A EJECUTAR
         add
@@ -57,7 +20,7 @@ map<string, int> registerMap {
         
 
 */
-using namespace std;
+int stringNumberToInt(const std::string&);
 
 class InstructionMemory : public sc_module
 {
@@ -74,10 +37,10 @@ private:
 
     void process();
 
-    string getInstruction();
-    void parseInstruction(string&, vector<string>& wordList);
-    string getFunctionType(const string&);
-
+    std::string getInstruction();
+    void parseInstruction(std::string&, std::vector<std::string>& wordList);
+    std::string getFunctionType(const std::string&);
+    void encodeInstruction(const std::string&, const std::vector<std::string>& );
 };
 
 
